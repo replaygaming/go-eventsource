574396ab5af566d6337d975e97604a3c4c07729f
Luiz Branco <me@luizbranco.com>
2015-06-26 13:47:41 -0400

    Round metrics time up by 1e3 to see on statsd

--- metrics.go
+++ metrics.go
@@ -9,6 +9,7 @@ import (
 )
 
 const sampleRate = 0.25
+const roundUpMs = 1e3
 
 type StatdsD struct {
 	prefix string
@@ -32,10 +33,10 @@ func (s StatdsD) ClientCount(count int) {
 
 func (s StatdsD) EventDone(e eventsource.Event, d time.Duration, c []time.Duration) {
 	s.statsd.Counter(1, s.prefix+"publish.count", 1)
-	s.statsd.Timing(sampleRate, s.prefix+"publish.timing", d)
+	s.statsd.Timing(sampleRate, s.prefix+"publish.timing", d*roundUpMs)
 	for _, t := range c {
 		if t > 0 {
-			s.statsd.Timing(sampleRate, s.prefix+"publish.connection_write.timing", t)
+			s.statsd.Timing(sampleRate, s.prefix+"publish.connection_write.timing", t*roundUpMs)
 		}
 	}
 }
