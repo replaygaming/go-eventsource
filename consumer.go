d916600435638aaf251d871f4ca8eee6a6e3ef4f
Luiz Branco <me@luizbranco.com>
2015-08-12 12:29:59 -0400

    Refactor consume to allow extraction

--- consumer.go
+++ consumer.go
@@ -1,28 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/streadway/amqp"
 )
 
-type consumer struct {
+// Consumer hh
+type Consumer struct {
 	conn    *amqp.Connection
 	channel *amqp.Channel
 	tag     string
 	done    chan error
 }
 
-func (c *consumer) subscribe(amqpURI string) (<-chan amqp.Delivery, error) {
-	c.conn = nil
-	c.channel = nil
-	c.done = make(chan error)
-
+// NewConsumer hh
+func NewConsumer(amqpURI, exchange, exchangeType, queueName, key, ctag string) (*Consumer, error) {
 	var err error
+	c := &Consumer{
+		conn:    nil,
+		channel: nil,
+		tag:     ctag,
+		done:    make(chan error),
+	}
 
 	c.conn, err = amqp.Dial(amqpURI)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Dial: %s", err)
 	}
 
 	go func() {
@@ -31,52 +36,59 @@ func (c *consumer) subscribe(amqpURI string) (<-chan amqp.Delivery, error) {
 
 	c.channel, err = c.conn.Channel()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Channel: %s", err)
 	}
 
-	err = c.channel.ExchangeDeclare(
-		"es_ex",  // name of the exchange
-		"fanout", // type
-		true,     // durable
-		false,    // delete when complete
-		false,    // internal
-		false,    // noWait
-		nil,      // arguments
-	)
-	if err != nil {
-		return nil, err
+	if err = c.channel.ExchangeDeclare(
+		exchange,     // name of the exchange
+		exchangeType, // type
+		true,         // durable
+		false,        // delete when complete
+		false,        // internal
+		false,        // noWait
+		nil,          // arguments
+	); err != nil {
+		return nil, fmt.Errorf("Exchange Declare: %s", err)
 	}
 
 	queue, err := c.channel.QueueDeclare(
-		"",    // name of the queue
-		true,  // durable
-		true,  // delete when usused
+		queueName, // name of the queue
+		true,      // durable
+		false,     // delete when usused
+		false,     // exclusive
+		false,     // noWait
+		nil,       // arguments
+	)
+	if err != nil {
+		return nil, fmt.Errorf("Queue Declare: %s", err)
+	}
+
+	if err = c.channel.QueueBind(
+		queue.Name, // name of the queue
+		key,        // bindingKey
+		exchange,   // sourceExchange
+		false,      // noWait
+		nil,        // arguments
+	); err != nil {
+		return nil, fmt.Errorf("Queue Bind: %s", err)
+	}
+
+	return c, nil
+}
+
+// Consume receives a queue name and returns a channel with the queue deliveries
+func (c *Consumer) Consume(queue string) (<-chan amqp.Delivery, error) {
+	deliveries, err := c.channel.Consume(
+		queue, // name
+		c.tag, // consumerTag,
+		false, // noAck
 		false, // exclusive
+		false, // noLocal
 		false, // noWait
 		nil,   // arguments
 	)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Queue Consume: %s", err)
 	}
-
-	err = c.channel.QueueBind(
-		queue.Name, // name of the queue
-		"es_ex",    // bindingKey
-		"es_ex",    // sourceExchange
-		false,      // noWait
-		nil,        // arguments
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	return c.channel.Consume(
-		queue.Name,    // name
-		"eventsource", // consumerTag,
-		false,         // noAck
-		false,         // exclusive
-		false,         // noLocal
-		false,         // noWait
-		nil,           // arguments
-	)
+	return deliveries, nil
 }
--- eventsource.go
+++ eventsource.go
@@ -11,8 +11,8 @@ import (
 var (
 	env       = flag.String("env", "development", "Environment: development or production")
 	port      = flag.String("port", "3001", "Eventsource port")
-	amqpUrl   = flag.String("amqp-url", "amqp://guest:guest@localhost:5672/eventsource", "AMQP URL")
-	statsdUrl = flag.String("statsd-url", "localhost:8125", "StatsD URL")
+	amqpURL   = flag.String("amqp-url", "amqp://guest:guest@localhost:5672/eventsource", "AMQP URL")
+	statsdURL = flag.String("statsd-url", "localhost:8125", "StatsD URL")
 	prefix    = flag.String("statsd-prefix", "app.es_go", "StatsD Prefix")
 	compress  = flag.Bool("compression", false, "Enable zlib compression of data")
 )
@@ -25,16 +25,19 @@ func main() {
 	server := &eventsource.Eventsource{
 		ChannelSubscriber: eventsource.QueryStringChannels{Name: "channels"},
 	}
-	metrics, err := NewMetrics(*statsdUrl, *prefix)
+	metrics, err := NewMetrics(*statsdURL, *prefix)
 	if err == nil && *env == "production" {
 		server.Metrics = metrics
 	}
 	server.Start()
 
-	c := consumer{}
-	messages, err := c.subscribe(*amqpUrl)
+	c, err := NewConsumer(*amqpURL, "es_ex", "fanout", "", "", "eventsource")
 	if err != nil {
-		log.Fatalf("[FATAL] AMQP %s", err)
+		log.Fatalf("[FATAL] AMQP consumer failed %s", err)
+	}
+	messages, err := c.Consume("")
+	if err != nil {
+		log.Fatalf("[FATAL] AMQP queue failed %s", err)
 	}
 
 	go func() {
@@ -57,7 +60,7 @@ func main() {
 
 	http.Handle("/subscribe", server)
 	log.Printf("[INFO] start env=%s port=%s amqp-url=%s statsd-url=%s statsd-prefix=%s"+
-		" compression=%t ", *env, *port, *amqpUrl, *statsdUrl, *prefix, *compress)
+		" compression=%t ", *env, *port, *amqpURL, *statsdURL, *prefix, *compress)
 	err = http.ListenAndServe(":"+*port, nil)
 	if err != nil {
 		log.Fatalf("[FATAL] Server %s", err)
