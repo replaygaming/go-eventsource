4b79a7ecfaecb8caf676789a8bd1b8838fa51d60
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@gmail.com>
2016-02-24 12:11:59 +0100

    Refactors main file, mostly method extraction

--- eventsource.go
+++ eventsource.go
@@ -1,77 +1,108 @@
 package main
 
 import (
-	"os"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/luizbranco/eventsource"
-	amqp "github.com/replaygaming/amqp-consumer"
+	amqpConsumer "github.com/replaygaming/amqp-consumer"
+	amqp "github.com/streadway/amqp"
 )
 
 var (
-	env				string
-	port			string
-	amqpURL		string
-	amqpQueue	string
-	statsdURL	string
-	prefix		string
-	compress 	bool
+	environment string
+	port        string
+	amqpURL     string
+	exchange    string
+	prefix      string
+	compress    bool
 )
 
 func init() {
-	env 		  = os.Getenv("ENV")
-	port      = os.Getenv("PORT")
-	amqpURL   = os.Getenv("AMQP_URL")
-	amqpQueue = os.Getenv("AMQP_QUEUE")
-	statsdURL = os.Getenv("STATSD_URL")
-	prefix    = "eventsource"
-	compress  = os.Getenv("COMPRESS") == "true"
+	environment = os.Getenv("ENV")
+	port = os.Getenv("PORT")
+	amqpURL = os.Getenv("AMQP_URL")
+	exchange = os.Getenv("EXCHANGE")
+	prefix = "eventsource"
+	compress = os.Getenv("COMPRESS") == "true"
 }
 
-func main() {
+func warn(message string, err error) {
+	log.Printf("[WARN] %s: %s", message, err)
+}
+
+func fatal(message string, err error) {
+	log.Fatalf("[FATAL] %s: %s", message, err)
+}
+
+func newServerWithMetrics(prefix string) *eventsource.Eventsource {
 	server := &eventsource.Eventsource{
 		ChannelSubscriber: eventsource.QueryStringChannels{Name: "channels"},
 	}
-	metrics, err := NewMetrics(statsdURL, prefix)
-	if err == nil && env == "production" {
+	metrics, err := NewMetrics(prefix)
+	if err != nil {
+		warn("Metrics proxy creation failed", err)
+	} else if environment == "production" {
 		server.Metrics = metrics
 	}
 	server.Start()
+	return server
+}
 
-	c, err := amqp.NewConsumer(amqpURL, "es_ex", "fanout", amqpQueue, "", "eventsource")
+func newConsumer(amqpURL string, exchange string) *amqpConsumer.Consumer {
+	// NewConsumer(amqpURI, exchange, exchangeType, queueName, key, ctag string) (*Consumer, error)
+	consumer, err := amqpConsumer.NewConsumer(amqpURL, exchange, "fanout", "", "", "eventsource")
 	if err != nil {
-		log.Fatalf("[FATAL] AMQP consumer failed %s", err)
+		fatal("AMQP consumer failed", err)
 	}
-	messages, err := c.Consume(amqpQueue)
-	if err != nil {
-		log.Fatalf("[FATAL] AMQP queue failed %s", err)
-	}
+	return consumer
+}
 
-	go func() {
-		for m := range messages {
-			data, channels, err := parseMessage(m.Body)
-			if err != nil {
-				log.Printf("[WARN] json conversion failed %s", err)
-			} else {
-				e := eventsource.DefaultEvent{
-					Message:  data,
-					Channels: channels,
-					Compress: compress,
-				}
-				server.Send(e)
+func consume(exchange string, consumer *amqpConsumer.Consumer) <-chan amqp.Delivery {
+	messages, err := consumer.Consume(exchange)
+	if err != nil {
+		fatal("AMQP queue failed", err)
+	}
+	return messages
+}
+
+func messageLoop(messages <-chan amqp.Delivery, server *eventsource.Eventsource, consumer *amqpConsumer.Consumer) {
+	for message := range messages {
+		data, channels, err := parseMessage(message.Body)
+		if err != nil {
+			warn("JSON conversion failed", err)
+		} else {
+			event := eventsource.DefaultEvent{
+				Message:  data,
+				Channels: channels,
+				Compress: compress,
 			}
-			m.Ack(false)
+			server.Send(event)
 		}
-		c.Done <- nil
-	}()
-
-	http.Handle("/subscribe", server)
-	log.Printf("[INFO] start env=%s port=%s amqp-url=%s amqp-queue=%s"+
-		" statsd-url=%s statsd-prefix=%s compression=%t", env, port, amqpURL,
-		amqpQueue, statsdURL, prefix, compress)
-	err = http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		log.Fatalf("[FATAL] Server %s", err)
+		message.Ack(false)
 	}
+	consumer.Done <- nil
+}
+
+func startServing(server *eventsource.Eventsource) {
+	http.Handle("/subscribe", server)
+
+	log.Printf("[INFO] STARTING - environment=%s, port=%s, AMPQ URL=%s, exchange=%s,"+
+		" stats prefix=%s, compression=%t", environment, port, amqpURL,
+		exchange, prefix, compress)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		fatal("Server", err)
+	}
+}
+
+func main() {
+	server := newServerWithMetrics(prefix)
+	consumer := newConsumer(amqpURL, exchange)
+	messages := consume(exchange, consumer)
+
+	go messageLoop(messages, server, consumer)
+
+	startServing(server)
 }
