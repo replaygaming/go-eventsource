9fc3ca8f4b1d8571f9c858e50f5da13162b11976
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@gmail.com>
2016-02-24 12:12:31 +0100

    Reimplements the metrics module in terms of Google cloud monitoring, WIP

--- metrics.go
+++ metrics.go
@@ -1,42 +1,80 @@
 package main
 
 import (
-	"strconv"
+	"log"
 	"time"
 
 	"github.com/luizbranco/eventsource"
-	"github.com/peterbourgon/g2s"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/cloudmonitoring/v2beta2"
 )
 
-const sampleRate = 0.25
-const roundUpMs = 1e3
+//const sampleRate = 0.25
+//const roundUpMs = 1e3
 
-type StatdsD struct {
-	prefix string
-	statsd g2s.Statter
+type GoogleCloudMonitoring struct {
+	base_url string
+	remote   cloudmonitoring.Service
 }
 
-func NewMetrics(addr string, prefix string) (StatdsD, error) {
-	stats := StatdsD{prefix: prefix + "."}
-	statsd, err := g2s.Dial("udp", addr)
+func NewMetrics(prefix string) (GoogleCloudMonitoring, error) {
+	monitor := GoogleCloudMonitoring{base_url: "custom.cloudmonitoring.googleapis.com/" + prefix + "/"}
+
+	// Use oauth2.NoContext if there isn't a good context to pass in.
+	ctx := context.Background()
+
+	client, err := google.DefaultClient(
+		ctx,
+		cloudmonitoring.MonitoringScope,
+	)
 	if err != nil {
-		return stats, err
+		log.Fatal(err)
 	}
-	stats.statsd = statsd
-	return stats, nil
+
+	cloudmonitoringService, err := cloudmonitoring.New(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	monitor.remote = *cloudmonitoringService
+	return monitor, nil
 }
 
-func (s StatdsD) ClientCount(count int) {
-	n := strconv.Itoa(count)
-	s.statsd.Gauge(1, s.prefix+"connections", n)
+func (monitor GoogleCloudMonitoring) ClientCount(count int) {
+	// n := strconv.Itoa(count)
+	// s.statsd.Gauge(1, s.prefix+"connections", n)
 }
 
-func (s StatdsD) EventDone(e eventsource.Event, d time.Duration, c []time.Duration) {
-	s.statsd.Counter(1, s.prefix+"publish.count", 1)
-	s.statsd.Timing(sampleRate, s.prefix+"publish.timing", d*roundUpMs)
-	for _, t := range c {
-		if t > 0 {
-			s.statsd.Timing(sampleRate, s.prefix+"publish.connection_write.timing", t*roundUpMs)
+func (monitor GoogleCloudMonitoring) EventDone(event eventsource.Event, duration time.Duration, eventdurations []time.Duration) {
+	// s.statsd.Counter(1, s.prefix+"publish.count", 1)
+	// s.statsd.Timing(sampleRate, s.prefix+"publish.timing", d*roundUpMs)
+	// for _, t := range c {
+	// 	if t > 0 {
+	// 		s.statsd.Timing(sampleRate, s.prefix+"publish.connection_write.timing", t*roundUpMs)
+	// 	}
+	// }
+
+	var sum float64
+	var count float64
+	var avg float64
+	for _, d := range eventdurations {
+		if d > 0 {
+			sum += float64(d)
+			count++
 		}
 	}
+	if count > 0 {
+		avg = sum / count
+	}
+	log.Printf("Event completed - clients %.f, avg time %.2f\n", count, avg)
 }
+
+//// If you need a oauth2.TokenSource, use the DefaultTokenSource function:
+
+//ts, err := google.DefaultTokenSource(ctx, scope1, scope2, ...)
+//if err != nil {
+//  // Handle error.
+//}
+//httpClient := oauth2.NewClient(ctx, ts)
