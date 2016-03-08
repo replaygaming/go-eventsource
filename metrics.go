c98dc5bb2498ff602a2ab292e9b182585e7893fa
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@gmail.com>
2016-03-08 11:25:20 +0100

    Adds real implementation for connection metric
    Implements the actual logic required to send metrics in Go. The implementation
    is VERY verbose, considering it will just create a JSON object in the other end.
    Compiler asses it atm. I'll implement the rest and then extract the common
    parts.
    
    WIP; I still need to implement the calls to ensure that a specific metric
    actually exist before we try to push values to it. This only has to be done once
    and I'm planning to put that into the initializer and keep it out of the
    frequently called metrics functions.

--- metrics.go
+++ metrics.go
@@ -16,36 +16,77 @@ import (
 
 type GoogleCloudMonitoring struct {
 	base_url string
-	remote   cloudmonitoring.Service
+	remote   cloudmonitoring.TimeseriesService
 }
 
 func NewMetrics(prefix string) (GoogleCloudMonitoring, error) {
 	monitor := GoogleCloudMonitoring{base_url: "custom.cloudmonitoring.googleapis.com/" + prefix + "/"}
 
-	// Use oauth2.NoContext if there isn't a good context to pass in.
-	ctx := context.Background()
-
 	client, err := google.DefaultClient(
-		ctx,
+		context.Background(),
 		cloudmonitoring.MonitoringScope,
 	)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Unable to get default client: %v", err)
 	}
 
 	cloudmonitoringService, err := cloudmonitoring.New(client)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Unable to create monitoring service: %v", err)
 	}
 
-	monitor.remote = *cloudmonitoringService
+	timeseriesService := cloudmonitoring.NewTimeseriesService(cloudmonitoringService)
+
+	monitor.remote = *timeseriesService
 	return monitor, nil
 }
 
 func (monitor GoogleCloudMonitoring) ClientCount(count int) {
 	// n := strconv.Itoa(count)
 	// s.statsd.Gauge(1, s.prefix+"connections", n)
-	log.Printf("[METRIC] %s.connections: %d\n", monitor.base_url, count)
+	log.Printf("[METRIC] %sconnections: %d\n", monitor.base_url, count)
+
+	count64 := int64( count )
+	now, err := time.Now().UTC().MarshalText()
+	if err != nil {
+		log.Fatal("ClientCount - Unable to get current time: %v", err)
+	}
+
+	description := cloudmonitoring.TimeseriesDescriptor{
+		Labels: map[string]string{
+			monitor.base_url + "implementation":"golang",
+		},
+    Metric: monitor.base_url + "connections",
+    Project: "replay-gaming",
+	}
+
+	point := cloudmonitoring.Point{
+		Start: string(now),
+		End: string(now),
+		Int64Value: &count64,
+	}
+
+	timeseries := cloudmonitoring.TimeseriesPoint{
+    Point: &point,
+  	TimeseriesDesc: &description,
+	}
+
+	points := []*cloudmonitoring.TimeseriesPoint{
+		&timeseries,
+	}
+
+	request := cloudmonitoring.WriteTimeseriesRequest{
+		CommonLabels: map[string]string{
+			"container.googleapis.com/container_name": "eventsource",
+		},
+		Timeseries: points,
+	}
+
+	response, err := monitor.remote.Write("replay-poker", &request).Do()
+	if err != nil {
+		log.Fatal("ClientCount - Unable to write timeseries: %v", err)
+	}
+	log.Printf("ClientCount - Response: %s", response)
 }
 
 func (monitor GoogleCloudMonitoring) EventDone(event eventsource.Event, duration time.Duration, eventdurations []time.Duration) {
