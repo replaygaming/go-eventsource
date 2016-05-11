f5be2285485fec70b8dfdc212ee960cc6c372670
Jonas Schubert Erlandsson <jonas.schubert.erlandsson@gmail.com>
2016-05-11 16:38:11 +0200

    Changes the Google metrics logger to a log based one for now.

--- metrics.go
+++ metrics.go
@@ -5,157 +5,23 @@ import (
 	"time"
 
 	"github.com/replaygaming/eventsource"
-
-	"golang.org/x/net/context"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/cloudmonitoring/v2beta2"
 )
 
-type GoogleCloudMonitoring struct {
-	base_url string
-	remote   cloudmonitoring.TimeseriesService
+type LogMonitoring struct {
+	prefix string
 }
 
-type Timeseries struct {
-	base_url    string
-	metric_name string
-	start       time.Time
-	end         time.Time
-	value       float64
-}
+func NewMetrics(prefix string) (LogMonitoring, error) {
+	monitor := LogMonitoring{prefix: prefix + "/"}
 
-func createTimeseries(args Timeseries) *cloudmonitoring.TimeseriesPoint {
-
-	var end_string string
-	var start_string string
-
-	start_string = args.start.Format(time.RFC3339)
-
-	if &args.end != nil {
-		end_string = args.end.Format(time.RFC3339)
-	} else {
-		end_string = start_string
-	}
-
-	description := cloudmonitoring.TimeseriesDescriptor{
-		Labels: map[string]string{
-			args.base_url + "implementation": "golang",
-		},
-		Metric:  args.base_url + args.metric_name,
-		Project: "replay-gaming",
-	}
-
-	point := cloudmonitoring.Point{
-		Start:       start_string,
-		End:         end_string,
-		DoubleValue: &args.value,
-	}
-
-	timeseries := cloudmonitoring.TimeseriesPoint{
-		Point:          &point,
-		TimeseriesDesc: &description,
-	}
-
-	return &timeseries
-}
-
-func pushMetrics(points []*cloudmonitoring.TimeseriesPoint, remote cloudmonitoring.TimeseriesService) {
-	request := cloudmonitoring.WriteTimeseriesRequest{
-		CommonLabels: map[string]string{
-			"container.googleapis.com/container_name": "eventsource",
-		},
-		Timeseries: points,
-	}
-
-	response, err := remote.Write("replay-gaming", &request).Do()
-	if err != nil {
-		log.Printf("pushMetrics - Unable to write timeseries: %v", err)
-	} else {
-		log.Printf("pushMetrics - Response: %s", response)
-	}
-}
-
-func createMetricDescriptor(prefix string, name string, description string) *cloudmonitoring.MetricDescriptor {
-	metric_type := cloudmonitoring.MetricDescriptorTypeDescriptor{
-		MetricType: "gauge",
-		ValueType:  "double",
-	}
-
-	label := cloudmonitoring.MetricDescriptorLabelDescriptor{
-		Description: "Application",
-		Key:         "custom.cloudmonitoring.googleapis.com/" + prefix + "/",
-	}
-
-	return &cloudmonitoring.MetricDescriptor{
-		Description: description,
-		Labels: []*cloudmonitoring.MetricDescriptorLabelDescriptor{
-			&label,
-		},
-		Name:           "custom.cloudmonitoring.googleapis.com/" + prefix + "/" + name,
-		Project:        "replay-gaming",
-		TypeDescriptor: &metric_type,
-	}
-}
-
-func createMetric(prefix string, metricDescriptorsService *cloudmonitoring.MetricDescriptorsService, name string, description string) {
-	metricDescriptor := createMetricDescriptor(prefix, name, description)
-	metricDescriptor, err := metricDescriptorsService.Create("replay-gaming", metricDescriptor).Do()
-	if err != nil {
-		log.Printf("Unable to create '%s' metric: %v", name, err)
-	}
-}
-
-func createMetrics(prefix string, cloudmonitoringService *cloudmonitoring.Service) {
-	metricDescriptorsService := cloudmonitoring.NewMetricDescriptorsService(cloudmonitoringService)
-
-	createMetric(prefix, metricDescriptorsService, "clients", "Number of clients that the event was distributed to")
-	createMetric(prefix, metricDescriptorsService, "avg_time", "Average time to send an event to all connected clients")
-	createMetric(prefix, metricDescriptorsService, "connections", "Number of connected SSE clients (browser sessions)")
-}
-
-func NewMetrics(prefix string) (GoogleCloudMonitoring, error) {
-	monitor := GoogleCloudMonitoring{base_url: "custom.cloudmonitoring.googleapis.com/" + prefix + "/"}
-
-	client, err := google.DefaultClient(
-		context.Background(),
-		cloudmonitoring.MonitoringScope,
-		cloudmonitoring.CloudPlatformScope,
-	)
-	if err != nil {
-		log.Printf("Unable to get default client: %v", err)
-	}
-
-	cloudmonitoringService, err := cloudmonitoring.New(client)
-	if err != nil {
-		log.Printf("Unable to create monitoring service: %v", err)
-	}
-
-	createMetrics(prefix, cloudmonitoringService)
-
-	timeseriesService := cloudmonitoring.NewTimeseriesService(cloudmonitoringService)
-
-	monitor.remote = *timeseriesService
 	return monitor, nil
 }
 
-func (monitor GoogleCloudMonitoring) ClientCount(count int) {
+func (monitor LogMonitoring) ClientCount(count int) {
 	log.Printf("[METRIC] %sconnections: %d\n", monitor.base_url, count)
-
-	timeseries := createTimeseries(Timeseries{
-		base_url:    monitor.base_url,
-		metric_name: "connections",
-		start:       time.Now().UTC(),
-		value:       float64(count),
-	})
-
-	points := []*cloudmonitoring.TimeseriesPoint{
-		timeseries,
-	}
-
-	pushMetrics(points, monitor.remote)
 }
 
-func (monitor GoogleCloudMonitoring) EventDone(event eventsource.Event, duration time.Duration, eventdurations []time.Duration) {
+func (monitor LogMonitoring) EventDone(event eventsource.Event, duration time.Duration, eventdurations []time.Duration) {
 	var sum int64
 	var count int64
 	var avg float64
@@ -174,25 +40,4 @@ func (monitor GoogleCloudMonitoring) EventDone(event eventsource.Event, duration
 
 	log.Printf("[METRIC] %s.event_distributed.clients: %d\n", monitor.base_url, count)
 	log.Printf("[METRIC] %s.event_distributed.avg_time: %dns\n", monitor.base_url, avg)
-
-	clients_timeseries := createTimeseries(Timeseries{
-		base_url:    monitor.base_url,
-		metric_name: "clients",
-		start:       time.Now().UTC(),
-		value:       float64(count),
-	})
-
-	avg_time_timeseries := createTimeseries(Timeseries{
-		base_url:    monitor.base_url,
-		metric_name: "avg_time",
-		start:       time.Now().UTC(),
-		value:       avg,
-	})
-
-	points := []*cloudmonitoring.TimeseriesPoint{
-		clients_timeseries,
-		avg_time_timeseries,
-	}
-
-	pushMetrics(points, monitor.remote)
 }
