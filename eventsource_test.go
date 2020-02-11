096f5b357074d6c62bc301e183b9a910a8d0f936
kvokka <kvokka@yahoo.com>
2020-02-11 02:24:37 +0000

    Move to internal go test framework

---
+++ eventsource_test.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	"cloud.google.com/go/pubsub"
+	"golang.org/x/net/context"
+)
+
+var _ = func() bool {
+	os.Setenv("PUBSUB_EMULATOR_HOST", fromEnvWithDefault("PUBSUB_EMULATOR_HOST", "pubsub-emulator:8538"))
+	testing.Init()
+	return true
+}()
+
+type EventSourceMessage struct {
+	Event    string   `json:"event"`
+	Data     string   `json:"data"`
+	Channels []string `json:"channels"`
+}
+
+var channels = "*"
+
+func sendMessageFromNewClient(event string, contents string) {
+	pubsubClient, _ := pubsub.NewClient(context.Background(), "emulator-project-id")
+	topic := pubsubClient.Topic("eventsource")
+
+	channelsList := strings.Split(channels, ",")
+
+	message := EventSourceMessage{
+		Event:    event,
+		Data:     contents,
+		Channels: channelsList,
+	}
+
+	payload, err := json.Marshal(message)
+	if err != nil {
+		log.Fatalf("Could not parse message: %v", err)
+	}
+
+	msgIDs, err := topic.Publish(context.Background(), &pubsub.Message{
+		Data: payload,
+	})
+	if err != nil {
+		fmt.Printf("Error publishing message %v\n", payload)
+	}
+	for _, id := range msgIDs {
+		fmt.Printf("Published a message; msg ID: %v\nPayload: %v\n", id, payload)
+	}
+}
+
+func makeRequest(url string, ch chan<- string) {
+	data := make([]byte, 10*1024)
+
+	res, err := http.Get(url)
+	if err != nil {
+		close(ch)
+		os.Exit(1)
+	}
+	defer res.Body.Close()
+	defer close(ch) //don't forget to close the channel as well
+
+	for n, err := res.Body.Read(data); err == nil; n, err = res.Body.Read(data) {
+		ch <- string(data[:n])
+	}
+}
+
+func TestMain(m *testing.M) {
+	go main()
+
+	time.Sleep(1 * time.Second)
+	ch := make(chan string)
+	go makeRequest("http://localhost/subscribe?channels=*,100", ch)
+	go makeRequest("http://localhost/subscribe?channels=*,200", ch)
+	go makeRequest("http://localhost/subscribe?channels=*,300", ch)
+
+	go func() {
+		time.Sleep(10 * time.Second)
+		fmt.Fprintf(os.Stderr, "Timeout: Did not get all messages!")
+		os.Exit(1)
+	}()
+
+	for i := 0; i < 10; i++ {
+		go sendMessageFromNewClient("test", "msg"+strconv.Itoa(i))
+	}
+
+	done := 0
+	for v := range ch {
+		for _, sub := range strings.Split(v, "\n") {
+			if strings.Contains(sub, "data") {
+				done++
+			}
+		}
+		if done == 30 {
+			break
+		}
+	}
+}
